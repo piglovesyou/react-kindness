59979a3c4b98541e7f603b853a82ae9d4b90da12
piglovesyou <thepiglovesyou@gmail.com>
2020-03-14 18:00:47 +0900

    wip: simplify scripts and directories

--- webpack.config.ts
+++ webpack.config.ts
@@ -10,7 +10,7 @@ const htmlWebpackPlugin = new HtmlWebpackPlugin({
 const config: webpack.Configuration = {
   entry: path.join(__dirname, 'examples/src/index.js'),
   output: {
-    path: path.join(__dirname, 'examples/dist'),
+    path: path.join(__dirname, 'demo'),
     filename: 'bundle.js'
   },
   module: {
