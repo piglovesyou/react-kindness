3d7a2fc62ffbdfb7a5b6949affdaa5f23cef25e8
piglovesyou <thepiglovesyou@gmail.com>
2020-03-14 18:45:26 +0900

    Support windows

--- karma.conf.ts
+++ karma.conf.ts
@@ -2,15 +2,22 @@
 // Generated on Sun Aug 05 2018 09:06:40 GMT+0900 (Japan Standard Time)
 
 import webpackConfig from './webpack.config';
+import { platform } from 'os';
 
 const isCI = process.env.CI === 'true';
+const browsers =
+  platform() === 'darwin'
+    ? 'Safari'
+    : platform().startsWith('win')
+    ? 'IE'
+    : isCI
+    ? 'Chrome'
+    : 'ChromeHeadless';
 
 const config = function(config) {
-
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
@@ -19,7 +26,6 @@ const config = function(config) {
       // 'karma-typescript'
     ],
 
-
     // list of files / patterns to load in the browser
     files: [
       // 'src/**/*.spec.js',
@@ -27,25 +33,20 @@ const config = function(config) {
       'src/**/*.spec.tsx',
     ],
 
-
     // list of files / patterns to exclude
-    exclude: [
-    ],
-
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.ts': [ 'webpack' ],
-      '**/*.tsx': [ 'webpack' ],
+      '**/*.ts': ['webpack'],
+      '**/*.tsx': ['webpack'],
     },
-
 
     webpack: {
       ...webpackConfig,
       mode: isCI ? 'production' : 'development',
     },
-
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
@@ -55,46 +56,36 @@ const config = function(config) {
       // 'karma-typescript',
     ],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: !isCI,
 
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [ isCI ? 'ChromeHeadless' : 'Chrome' ],
-    // browsers: [ 'ChromeHeadless' ],
-
+    browsers,
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: isCI,
 
-
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity,
 
-
     karmaTypescriptConfig: {
-      tsconfig: "tsconfig.json"
+      tsconfig: 'tsconfig.json',
     },
 
-
-    browserNoActivityTimeout: 10 *1000,
-  })
+    browserNoActivityTimeout: 10 * 1000,
+  });
 };
 export default config;
